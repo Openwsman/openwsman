98095209a5b1f35d6089b0f56483f46f87fdee8a
Anas Nashif <nashif@intel.com>
2006-12-08 14:45:31 +0000

    xml serialization tests

--- tests/xml/xml1.c
+++ tests/xml/xml1.c
@@ -5,24 +5,53 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <glib.h>
-
-#include "ws_utilities.h"
+#include "u/libu.h"
 
 
-#include "ws_xml_api.h"
-#include "ws_errors.h"
-#include "soap_api.h"
-#include "xml_api_generic.h"
-#include "xml_serializer.h"
+#include "wsman-soap.h"
+#include "wsman-xml.h"
+#include "wsman-errors.h"
+#include "wsman-soap.h"
+#include "wsman-xml-serializer.h"
 
+
+struct __CIM_ComputerSystem {
+    char *NameFormat;
+    char *test[2];
+    XmlSerialiseDynamicSizeData* foo;
+};
+typedef struct __CIM_ComputerSystem CIM_ComputerSystem;
+
+struct __XmlSerializerInfo foo_TypeInfo[] =
+{
+      SER_STR("foo", 1, 1)
+};
+
+SER_START_ITEMS("CIM_ComputerSystem", CIM_ComputerSystem)
+SER_STR("NameFormat",0,1),
+SER_STR("test", 0, 2 ),
+SER_DYN_ARRAY_PTR(foo),
+SER_END_ITEMS("CIM_ComputerSystem", CIM_ComputerSystem);
+
+
+#define NS "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ComputerSystem"
 
 int main(void)
 {
-    
     WsContextH cntx = ws_create_runtime(NULL);
+    SoapH soap = ws_context_get_runtime(cntx);
+    
+    WsXmlDocH doc = ws_xml_read_file(soap, "cim_computersystem_01.xml", "UTF-8", 0 ); 
+    WsXmlNodeH node = ws_xml_get_soap_body(doc);
+    CIM_ComputerSystem *cs  = ws_deserialize(cntx,
+                                     node,
+                                     CIM_ComputerSystem_TypeInfo,"CIM_ComputerSystem",
+                                     NS, NS,
+                                     0, 0);
 
-    ws_xml_parser_destroy((SoapH)cntx);
-    soap_free(cntx);
+    printf("%s\n", cs->NameFormat);
+    printf("%s\n", cs->test[1]);
+    printf("%d\n", cs->foo->count);
+
     return 0;
 }
--- tests/xml/xml2.c
+++ tests/xml/xml2.c
@@ -5,38 +5,19 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <glib.h>
-
-#include "ws_utilities.h"
+#include "u/libu.h"
 
 
-#include "ws_xml_api.h"
-#include "ws_errors.h"
-#include "soap_api.h"
-#include "xml_api_generic.h"
-#include "xml_serializer.h"
+#include "wsman-xml-api.h"
+#include "wsman-errors.h"
+#include "wsman-soap.h"
+#include "wsman-xml-serializer.h"
+
+
 
 
 int main(void)
 {
     
-    g_type_init ();
-    g_thread_init (NULL);
-    WsContextH cntx = ws_create_runtime(NULL);
-
-    SOAP_FW* soap = (SOAP_FW*)ws_context_get_runtime(cntx);
-    WsXmlDocH doc = ws_xml_read_file (soap, "./data/xml_test1.xml", NULL, 0);
-    if (doc) {
-         ws_xml_dump_node_tree(stdout, ws_xml_get_doc_root(doc));
-    }
-    
-    char* nameNsPrefix = ws_xml_get_node_name_ns_prefix(ws_xml_get_child(ws_xml_get_soap_body(doc), 0 , NULL, NULL));
-    char* Uri = ws_xml_get_node_name_ns_uri(ws_xml_get_child(ws_xml_get_soap_body(doc), 0 , NULL, NULL));
-    fprintf(stdout, "\nnamespace: %s\n",  nameNsPrefix);
-    fprintf(stdout, "namespace uri: %s\n",  Uri);
-
-    
-    //ws_xml_parser_destroy((SoapH)cntx);
-    //soap_free(cntx);
     return 0;
 }
