b062e453d0db3ecdd4ae9048dab96a31c7d58690
Anas Nashif <nashif@intel.com>
2008-04-22 00:01:23 +0000

    style

--- src/lib/wsman-win-client-transport.c
+++ src/lib/wsman-win-client-transport.c
@@ -68,10 +68,10 @@
 /* ensure that the winhttp library is linked */
 #pragma comment( lib, "winhttp.lib" )
 static BOOL find_cert(const _TCHAR * oid,
-					  const _TCHAR * certName,
-					  BOOL localMachine,
-					  PCCERT_CONTEXT  *pCertContext,
-					  int* errorLast);
+		const _TCHAR * certName,
+		BOOL localMachine,
+		PCCERT_CONTEXT  *pCertContext,
+		int* errorLast);
 void wsman_client_handler( WsManClient *cl, WsXmlDocH rqstDoc, void* user_data);
 
 
@@ -83,7 +83,7 @@ static wchar_t *convert_to_unicode(char *str)
 		return NULL;
 	}
 	unicode_str =
-	    (wchar_t *) malloc((strlen(str) + 1) * sizeof(wchar_t));
+		(wchar_t *) malloc((strlen(str) + 1) * sizeof(wchar_t));
 	if (unicode_str) {
 		if (mbstowcs(unicode_str, str, strlen(str) + 1) <= 0) {
 			error("No -> Unicode: %s", str);
@@ -115,22 +115,22 @@ int wsmc_transport_init(WsManClient *cl, void *arg)
 	}
 	if(!cl->proxy_data.proxy){
 
-                cl->session_handle = WinHttpOpen(agent,
-                                          WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
-                                          WINHTTP_NO_PROXY_NAME,
-                                          WINHTTP_NO_PROXY_BYPASS, 0);
-        }
-        else
-        {
-                proxy = convert_to_unicode(cl->proxy_data.proxy);
-                cl->session_handle = WinHttpOpen(agent,
-                                          WINHTTP_ACCESS_TYPE_NAMED_PROXY,
-                                          proxy,
-                                          WINHTTP_NO_PROXY_BYPASS, 0);
-                if (proxy)
-                        u_free(proxy);
+		cl->session_handle = WinHttpOpen(agent,
+				WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+				WINHTTP_NO_PROXY_NAME,
+				WINHTTP_NO_PROXY_BYPASS, 0);
+	}
+	else
+	{
+		proxy = convert_to_unicode(cl->proxy_data.proxy);
+		cl->session_handle = WinHttpOpen(agent,
+				WINHTTP_ACCESS_TYPE_NAMED_PROXY,
+				proxy,
+				WINHTTP_NO_PROXY_BYPASS, 0);
+		if (proxy)
+			u_free(proxy);
 
-        }
+	}
 
 	cl->lock_session_handle = 0L;
 	u_free(agent);
@@ -223,19 +223,19 @@ static DWORD ChooseAuthScheme(DWORD dwSupportedSchemes, int ws_auth)
 static DWORD Auth2Scheme(int ws_auth)
 {
 	if (ws_auth == WS_GSSNEGOTIATE_AUTH) {
-			return WINHTTP_AUTH_SCHEME_NEGOTIATE;
+		return WINHTTP_AUTH_SCHEME_NEGOTIATE;
 	}
 	if (ws_auth == WS_NTLM_AUTH) {
-			return WINHTTP_AUTH_SCHEME_NTLM;
+		return WINHTTP_AUTH_SCHEME_NTLM;
 	}
 	if (ws_auth == WS_PASS_AUTH) {
-			return WINHTTP_AUTH_SCHEME_PASSPORT;
+		return WINHTTP_AUTH_SCHEME_PASSPORT;
 	}
 	if (ws_auth == WS_DIGEST_AUTH) {
-			return WINHTTP_AUTH_SCHEME_DIGEST;
+		return WINHTTP_AUTH_SCHEME_DIGEST;
 	}
 	if (ws_auth == WS_BASIC_AUTH) {
-			return WINHTTP_AUTH_SCHEME_BASIC;
+		return WINHTTP_AUTH_SCHEME_BASIC;
 	}
 	return 0;
 }
@@ -251,14 +251,14 @@ static int cleanup_request_data(HINTERNET request)
 		dwSize = 0;
 		if (!WinHttpQueryDataAvailable(request, &dwSize)) {
 			error("Error %u in WinHttpQueryDataAvailable.",
-			      GetLastError());
+					GetLastError());
 			return 1;
 		}
 		dwSize = (dwSize > BUFLEN) ? BUFLEN : dwSize;
 		if (!WinHttpReadData(request, (LPVOID) buffer,
-				     dwSize, &dwDownloaded)) {
+					dwSize, &dwDownloaded)) {
 			error("Error %u in WinHttpReadData.",
-			      GetLastError());
+					GetLastError());
 			return 1;
 		}
 		if (dwDownloaded == 0) {
@@ -331,9 +331,9 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 
 	pwsz = convert_to_unicode(cl->data.path);
 	request =
-	    WinHttpOpenRequest(connect, L"POST", pwsz, L"HTTP/1.1",
-			       WINHTTP_NO_REFERER,
-			       WINHTTP_DEFAULT_ACCEPT_TYPES, flags);
+		WinHttpOpenRequest(connect, L"POST", pwsz, L"HTTP/1.1",
+				WINHTTP_NO_REFERER,
+				WINHTTP_DEFAULT_ACCEPT_TYPES, flags);
 	u_free(pwsz);
 	if (request == NULL) {
 		dwStatusCode = 400;
@@ -342,9 +342,9 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 	snprintf(pszAnsi, 128, "Content-Type: application/soap+xml;charset=%s\r\n", cl->content_encoding);
 	pwsz = convert_to_unicode(pszAnsi);
 	bResult = WinHttpAddRequestHeaders(request,
-					   pwsz,
-					   -1,
-					   WINHTTP_ADDREQ_FLAG_ADD_IF_NEW);
+			pwsz,
+			-1,
+			WINHTTP_ADDREQ_FLAG_ADD_IF_NEW);
 	u_free(pwsz);
 	if (!bResult) {
 		error("can't add Content-Type header");
@@ -359,10 +359,10 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 	{
 		DWORD d = WINHTTP_ENABLE_SPN_SERVER_PORT;
 		bResults =WinHttpSetOption(request,
-				      WINHTTP_OPTION_SPN,
-									(LPVOID) (&d),
-									sizeof(DWORD));
-	    if (!bResults)
+				WINHTTP_OPTION_SPN,
+				(LPVOID) (&d),
+				sizeof(DWORD));
+		if (!bResults)
 		{
 			lastErr = GetLastError();
 			bDone = TRUE;
@@ -371,52 +371,52 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 		bResults = FALSE;
 	}
 	if(cl->proxy_data.proxy_username)
-        {
-                proxy_username = convert_to_unicode(cl->proxy_data.proxy_username);
-                bResults = WinHttpSetOption(request, WINHTTP_OPTION_PROXY_USERNAME,
-                                                                        proxy_username, wcslen(proxy_username));
-                u_free(proxy_username);
-                if (!bResults)
-                {
-                        lastErr = GetLastError();
-                        bDone = TRUE;
+	{
+		proxy_username = convert_to_unicode(cl->proxy_data.proxy_username);
+		bResults = WinHttpSetOption(request, WINHTTP_OPTION_PROXY_USERNAME,
+				proxy_username, wcslen(proxy_username));
+		u_free(proxy_username);
+		if (!bResults)
+		{
+			lastErr = GetLastError();
+			bDone = TRUE;
 
-                }
-                bResults = FALSE;
-        }
-	 if(cl->proxy_data.proxy_password)
-        {
-                proxy_password = convert_to_unicode(cl->proxy_data.proxy_password);
-                bResults = WinHttpSetOption(request, WINHTTP_OPTION_PROXY_PASSWORD,
-                                                                        proxy_password, wcslen(proxy_password));
-                u_free(proxy_password);
-                if (!bResults)
-                {
-                        lastErr = GetLastError();
-                        bDone = TRUE;
+		}
+		bResults = FALSE;
+	}
+	if(cl->proxy_data.proxy_password)
+	{
+		proxy_password = convert_to_unicode(cl->proxy_data.proxy_password);
+		bResults = WinHttpSetOption(request, WINHTTP_OPTION_PROXY_PASSWORD,
+				proxy_password, wcslen(proxy_password));
+		u_free(proxy_password);
+		if (!bResults)
+		{
+			lastErr = GetLastError();
+			bDone = TRUE;
 
-                }
-                bResults = FALSE;
-        }
-	 if(0==cl->authentication.verify_host || 0==cl->authentication.verify_peer)
-	 {
+		}
+		bResults = FALSE;
+	}
+	if(0==cl->authentication.verify_host || 0==cl->authentication.verify_peer)
+	{
 		if(0==cl->authentication.verify_host)
 			flags = flags | SECURITY_FLAG_IGNORE_CERT_CN_INVALID;
 		if(0==cl->authentication.verify_peer)
 			flags = flags | SECURITY_FLAG_IGNORE_CERT_DATE_INVALID | SECURITY_FLAG_IGNORE_UNKNOWN_CA;
 		bResult = WinHttpSetOption(request,WINHTTP_OPTION_SECURITY_FLAGS,(LPVOID) (&flags),sizeof(DWORD));
-        if (!bResult) {
-			  //log the error and proceed
-              error("cannot ignore server certificate");
-        }
-	 }
+		if (!bResult) {
+			//log the error and proceed
+			error("cannot ignore server certificate");
+		}
+	}
 	while (!bDone) {
 		bResult = WinHttpSendRequest(request,
-					     WINHTTP_NO_ADDITIONAL_HEADERS,
-					     (DWORD) 0, (LPVOID) buf,
-					     (DWORD) errLen,
-					     (DWORD) errLen,
-					     (DWORD_PTR) NULL);
+				WINHTTP_NO_ADDITIONAL_HEADERS,
+				(DWORD) 0, (LPVOID) buf,
+				(DWORD) errLen,
+				(DWORD) errLen,
+				(DWORD_PTR) NULL);
 		if (bResult) {
 			bResults = WinHttpReceiveResponse(request, NULL);
 		}
@@ -432,11 +432,11 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 		// Check the status code.
 		if (bResults) {
 			bResults = WinHttpQueryHeaders(request,
-						       WINHTTP_QUERY_STATUS_CODE
-						       |
-						       WINHTTP_QUERY_FLAG_NUMBER,
-						       WINHTTP_HEADER_NAME_BY_INDEX, &dwStatusCode,
-						       &dwSize, WINHTTP_NO_HEADER_INDEX);
+					WINHTTP_QUERY_STATUS_CODE
+					|
+					WINHTTP_QUERY_FLAG_NUMBER,
+					WINHTTP_HEADER_NAME_BY_INDEX, &dwStatusCode,
+					&dwSize, WINHTTP_NO_HEADER_INDEX);
 		}
 		if (!bResults) {
 			if (updated) {
@@ -444,12 +444,12 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 				break;
 			}
 			if (ERROR_WINHTTP_CLIENT_AUTH_CERT_NEEDED ==
-			    lastErr) {
+					lastErr) {
 				lastErr = 0;
 
-			if (!find_cert(	(const _TCHAR *)  cl->authentication.caoid,
-				(const _TCHAR *)  cl->authentication.cainfo,
-				cl->authentication.calocal, &certificate, &lastErr)) {
+				if (!find_cert(	(const _TCHAR *)  cl->authentication.caoid,
+							(const _TCHAR *)  cl->authentication.cainfo,
+							cl->authentication.calocal, &certificate, &lastErr)) {
 					debug("No certificate");
 
 					bDone = TRUE;
@@ -457,11 +457,11 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 				}
 
 				bResults = WinHttpSetOption(request,
-							    WINHTTP_OPTION_CLIENT_CERT_CONTEXT,
-							    (LPVOID)
-							    certificate,
-							    (DWORD) (sizeof
-								     (CERT_CONTEXT)));
+						WINHTTP_OPTION_CLIENT_CERT_CONTEXT,
+						(LPVOID)
+						certificate,
+						(DWORD) (sizeof
+							(CERT_CONTEXT)));
 				if (!bResults) {
 					lastErr = GetLastError();
 					bDone = TRUE;
@@ -474,22 +474,22 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 			} else {
 
 				bResults = WinHttpQueryAuthSchemes(request,
-								   &dwSupportedSchemes,
-								   &dwFirstScheme,
-								   &dwTarget);
+						&dwSupportedSchemes,
+						&dwFirstScheme,
+						&dwTarget);
 
 				// Set the credentials before resending the request.
 				if (bResults) {
 					if (WS_MAX_AUTH == ws_auth || !cl->data.user ||
-						!cl->data.pwd) {
+							!cl->data.pwd) {
 						// we don't have credentials
 						bDone = TRUE;
 						bResults = 0;
-				break;
-			}
+						break;
+					}
 					dwSelectedScheme =
 						ChooseAuthScheme(dwSupportedSchemes,
-								 ws_auth);
+								ws_auth);
 
 					if (dwSelectedScheme == 0) {
 						bDone = TRUE;
@@ -504,10 +504,10 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 						break;
 					}
 					bResults = WinHttpSetCredentials(request,
-									 dwTarget,
-									 dwSelectedScheme,
-									 usr, pwd,
-									 NULL);
+							dwTarget,
+							dwSelectedScheme,
+							usr, pwd,
+							NULL);
 					u_free(pwd);
 					u_free(usr);
 				}
@@ -531,12 +531,12 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 			break;
 		case 400:
 			debug("Error. Status code %d returned.",
-			      dwStatusCode);
+					dwStatusCode);
 			bDone = TRUE;
 			break;
 		case 500:
 			debug("Error. Status code %d returned.",
-			      dwStatusCode);
+					dwStatusCode);
 			bDone = TRUE;
 			break;
 		case 401:
@@ -555,10 +555,10 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 				DWORD data = WINHTTP_AUTOLOGON_SECURITY_LEVEL_MEDIUM;
 				DWORD dataSize = sizeof(DWORD);
 				bResults = WinHttpSetOption(
-					request,
-					WINHTTP_OPTION_AUTOLOGON_POLICY,
-					&data,
-					dataSize);
+						request,
+						WINHTTP_OPTION_AUTOLOGON_POLICY,
+						&data,
+						dataSize);
 				if (!bResults)
 				{
 					lastErr = GetLastError();
@@ -566,29 +566,29 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 
 				}
 				if (cleanup_request_data(request)) {
-				// the problems to read data
-				bDone = TRUE;
+					// the problems to read data
+					bDone = TRUE;
 				}
 				break;
 			}
 
 			bResults = WinHttpQueryAuthSchemes(request,
-							   &dwSupportedSchemes,
-							   &dwFirstScheme,
-							   &dwTarget);
+					&dwSupportedSchemes,
+					&dwFirstScheme,
+					&dwTarget);
 
 			// Set the credentials before resending the request.
 			if (bResults) {
 				if (WS_MAX_AUTH == ws_auth || !cl->data.user ||
-				    !cl->data.pwd) {
+						!cl->data.pwd) {
 					// we don't have credentials
 					bDone = TRUE;
 					bResults = 0;
 					break;
 				}
 				dwSelectedScheme =
-				    ChooseAuthScheme(dwSupportedSchemes,
-						     ws_auth);
+					ChooseAuthScheme(dwSupportedSchemes,
+							ws_auth);
 
 				if (dwSelectedScheme == 0) {
 					bDone = TRUE;
@@ -603,10 +603,10 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 					break;
 				}
 				bResults = WinHttpSetCredentials(request,
-								 dwTarget,
-								 dwSelectedScheme,
-								 usr, pwd,
-								 NULL);
+						dwTarget,
+						dwSelectedScheme,
+						usr, pwd,
+						NULL);
 				u_free(pwd);
 				u_free(usr);
 			}
@@ -615,31 +615,31 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 				bDone = TRUE;
 			}
 			break;
-/*
-        case 407:
-          // The proxy requires authentication.
+			/*
+			   case 407:
+			// The proxy requires authentication.
 
-          // Obtain the supported and preferred schemes.
-          bResults = WinHttpQueryAuthSchemes( hRequest,
-                                              &dwSupportedSchemes,
-                                              &dwFirstScheme,
-                                              &dwTarget );
+			// Obtain the supported and preferred schemes.
+			bResults = WinHttpQueryAuthSchemes( hRequest,
+			&dwSupportedSchemes,
+			&dwFirstScheme,
+			&dwTarget );
 
-          // Set the credentials before resending the request.
-          if( bResults )
-            dwProxyAuthScheme = ChooseAuthScheme(dwSupportedSchemes);
+			// Set the credentials before resending the request.
+			if( bResults )
+			dwProxyAuthScheme = ChooseAuthScheme(dwSupportedSchemes);
 
-          // If the same credentials are requested twice, abort the
-          // request.  For simplicity, this sample does not check
-          // for a repeated sequence of status codes.
-          if( dwLastStatus == 407 )
-            bDone = TRUE;
-          break;
-*/
+			// If the same credentials are requested twice, abort the
+			// request.  For simplicity, this sample does not check
+			// for a repeated sequence of status codes.
+			if( dwLastStatus == 407 )
+			bDone = TRUE;
+			break;
+			*/
 		default:
 			// The status code does not indicate success.
 			debug("Error. Status code %d returned.",
-			      dwStatusCode);
+					dwStatusCode);
 			bResults = 0;
 			bDone = TRUE;
 		}
@@ -666,7 +666,7 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 		if (!bResults) {
 			lastErr = GetLastError();
 			error("Error %u in WinHttpQueryDataAvailable.",
-			      lastErr);
+					lastErr);
 			break;
 		}
 		if (dwSize > 0) {
@@ -675,8 +675,8 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 		p = (char *) u_buf_ptr(ubuf);
 		len = u_buf_len(ubuf);
 		bResults = WinHttpReadData(request,
-					   (LPVOID) (p + len), dwSize,
-					   &dwDownloaded);
+				(LPVOID) (p + len), dwSize,
+				&dwDownloaded);
 		if (!bResults) {
 			lastErr = GetLastError();
 			error("Error %u in WinHttpReadData.", lastErr);
@@ -688,7 +688,7 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 		}
 		u_buf_set_len(ubuf, len + dwDownloaded);
 	}
-      DONE:
+DONE:
 	cl->response_code = dwStatusCode;
 	cl->last_error = lastErr;
 	ws_xml_free_memory(buf);
@@ -699,10 +699,10 @@ wsmc_handler(WsManClient * cl, WsXmlDocH rqstDoc, void *user_data)
 
 // in future change this to return a list of certs...
 BOOL find_cert(const _TCHAR * oid,
-					  const _TCHAR * certName,
-					  BOOL localMachine,
-					  PCCERT_CONTEXT  *pCertContext,
-					  int* errorLast)
+		const _TCHAR * certName,
+		BOOL localMachine,
+		PCCERT_CONTEXT  *pCertContext,
+		int* errorLast)
 {
 
 	_TCHAR pszNameString[CERT_MAX_STR_LEN];
@@ -725,12 +725,12 @@ BOOL find_cert(const _TCHAR * oid,
 
 	/* Open the personal store */
 	if ( !(hStoreHandle = CertOpenStore(
-		CERT_STORE_PROV_SYSTEM,          // The store provider type
-		0,                               // The encoding type is not needed
-		NULL,                            // Use the default HCRYPTPROV
-		flags,  // Set the store location in a registry location
-		L"MY"                            // The store name as a Unicode string
-		)))
+					CERT_STORE_PROV_SYSTEM,          // The store provider type
+					0,                               // The encoding type is not needed
+					NULL,                            // Use the default HCRYPTPROV
+					flags,  // Set the store location in a registry location
+					L"MY"                            // The store name as a Unicode string
+					)))
 	{
 		/* Cannot open the certificates store - exit immediately */
 		lastErr = GetLastError();
@@ -741,25 +741,25 @@ BOOL find_cert(const _TCHAR * oid,
 	// Search for the first client certificate
 
 	*pCertContext = CertFindCertificateInStore(
-		hStoreHandle,
-		X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
-						   CERT_FIND_EXT_ONLY_ENHKEY_USAGE_FLAG,
-						   CERT_FIND_ENHKEY_USAGE,
-        &od,
-        NULL);
+			hStoreHandle,
+			X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
+			CERT_FIND_EXT_ONLY_ENHKEY_USAGE_FLAG,
+			CERT_FIND_ENHKEY_USAGE,
+			&od,
+			NULL);
 
 	/*
 	   If certificate was found - determinate its name. Keep search
 	   while the certificate's Common Name doesn't match the name
 	   defined by the user
-	 */
+	   */
 	while (*pCertContext != NULL) {
 		if (!CertGetNameString(*pCertContext,
-				       CERT_NAME_SIMPLE_DISPLAY_TYPE,
-				       0,
-				       NULL,
-				       pszNameString,
-				       CERT_MAX_STR_LEN - 1)) {
+					CERT_NAME_SIMPLE_DISPLAY_TYPE,
+					0,
+					NULL,
+					pszNameString,
+					CERT_MAX_STR_LEN - 1)) {
 			/* obtaining certificate name failed */
 			lastErr = GetLastError();
 			error("error %d in CertGetNameString.", lastErr);
@@ -767,25 +767,25 @@ BOOL find_cert(const _TCHAR * oid,
 		}
 
 		if (certName == NULL ||
-		    _tcscmp(pszNameString, certName) == 0) {
+				_tcscmp(pszNameString, certName) == 0) {
 			certSuccess = TRUE;
 			break;
 		}
 
 		*pCertContext =
-		    CertFindCertificateInStore(hStoreHandle,
-					       X509_ASN_ENCODING |
-					       PKCS_7_ASN_ENCODING,
-					       CERT_FIND_EXT_ONLY_ENHKEY_USAGE_FLAG,
-					       CERT_FIND_ENHKEY_USAGE,
-					       &od, *pCertContext);
+			CertFindCertificateInStore(hStoreHandle,
+					X509_ASN_ENCODING |
+					PKCS_7_ASN_ENCODING,
+					CERT_FIND_EXT_ONLY_ENHKEY_USAGE_FLAG,
+					CERT_FIND_ENHKEY_USAGE,
+					&od, *pCertContext);
 
 	}
 
 	if (*pCertContext == NULL) {
 		lastErr = GetLastError();
 		error("error %d (%s) in CertFindCertificateInStore.",
-		      lastErr);
+				lastErr);
 	}
 	if (errorLast) {
 		*errorLast = lastErr;
