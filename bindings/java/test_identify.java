c5ff17aa1302e705d083c5bc1827eaca2bc8e9a0
Anas Nashif <nashif@intel.com>
2008-05-28 17:17:27 +0000

    added test/example

---
+++ bindings/java/test_identify.java
@@ -0,0 +1,19 @@
+public class test_identify {
+  static {
+    try {
+	System.loadLibrary("jwsman");
+    } catch (UnsatisfiedLinkError e) {
+      System.err.println("Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n" + e);
+      System.exit(1);
+    }
+  }
+
+  public static void main(String argv[]) {
+	  Client c = new Client("http://wsman:secret@localhost:8889/wsman");
+
+	  ClientOptions op = new ClientOptions();
+	  WsXmlDoc doc = c.identify(op);
+	  System.out.println(doc.dump("UTF8"));
+  }
+}
+
