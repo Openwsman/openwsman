6b1a47fcec33e96b1cf5d95c0d9fcd74394f8a27
Klaus Kaempf <kkaempf@novell.com>
2008-11-08 22:37:25 +0000

    add correct parameters to functions

--- src/plugins/swig/ruby/rbwsmanplugin.rb
+++ src/plugins/swig/ruby/rbwsmanplugin.rb
@@ -4,53 +4,45 @@
 # Generic openwsman server plugin
 #
 
-module WsmanPlugin
+module Rbwsman
   def self.create_plugin
     Sample.new
   end
   class Sample
-  SCHEMA = "http://schema.opensuse.org/swig/wsman-schema/1-0"
-  def initialize *args
-    STDERR.puts "WsmanPlugin.new #{args}"
-  end
-  def namespaces
-    [ [SCHEMA, "Ruby"] ]
-  end
-  def identify
-    STDERR.puts "WsmanPlugin.identify"
-    0
-  end
-  def enumerate
-    STDERR.puts "WsmanPlugin.enumerate"
-    0
-  end
-  def release
-    STDERR.puts "WsmanPlugin.release"
-    0
-  end
-  def pull
-    STDERR.puts "WsmanPlugin.pull"
-    0
-  end
-  def get
-    STDERR.puts "WsmanPlugin.get"
-    0
-  end
-  def custom
-    STDERR.puts "WsmanPlugin.custom"
-    0
-  end
-  def put
-    STDERR.puts "WsmanPlugin.put"
-    0
-  end
-  def create
-    STDERR.puts "WsmanPlugin.create"
-    0
-  end
-  def delete
-    STDERR.puts "WsmanPlugin.delete"
-    0
-  end
+    SCHEMA = "http://schema.opensuse.org/swig/wsman-schema/1-0"
+    def initialize *args
+      STDERR.puts "WsmanPlugin.new #{args}"
+    end
+    def namespaces
+      [ [SCHEMA, "Ruby"] ]
+    end
+    def identify context
+      STDERR.puts "WsmanPlugin.identify, context #{context}"
+    end
+    def enumerate context, enum_info, status
+      
+      STDERR.puts "WsmanPlugin.enumerate, context #{context}"
+    end
+    def release context, enum_info, status
+      STDERR.puts "WsmanPlugin.release, context #{context}"
+    end
+    def pull context, enum_info, status
+      STDERR.puts "WsmanPlugin.pull, op #{op}"
+    end
+    def get op
+      STDERR.puts "WsmanPlugin.get, op #{op}"
+    end
+    def custom op
+      STDERR.puts "WsmanPlugin.custom, op #{op}"
+    end
+    def put op
+      STDERR.puts "WsmanPlugin.put, op #{op}"
+    end
+    def create op
+      STDERR.puts "WsmanPlugin.create, op #{op}"
+    end
+    def delete op
+      STDERR.puts "WsmanPlugin.delete, op #{op}"
+    end
 end
 end
