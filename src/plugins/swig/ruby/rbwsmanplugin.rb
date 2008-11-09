1f6d3ddc32ef34a8ecd61bad99698c18e7eaad73
Klaus Kaempf <kkaempf@novell.com>
2008-11-09 10:57:08 +0000

    make callbacks per-module, klass isn't passed to the plugin endpoints

--- src/plugins/swig/ruby/rbwsmanplugin.rb
+++ src/plugins/swig/ruby/rbwsmanplugin.rb
@@ -8,6 +8,36 @@ module Rbwsman
   def self.create_plugin
     Sample.new
   end
+  def self.identify context
+    STDERR.puts "WsmanPlugin.identify, context #{context}"
+  end
+  def self.enumerate context, enum_info, status
+    STDERR.puts "WsmanPlugin.enumerate, context #{context}, enum_info #{enum_info}"
+    STDERR.puts "to #{enum_info.epr_to}, uri #{enum_info.epr_uri}"
+    selectors = context.selectors
+    STDERR.puts "selectors #{selectors}"
+  end
+  def self.release context, enum_info, status
+    STDERR.puts "WsmanPlugin.release, context #{context}"
+  end
+  def self.pull context, enum_info, status
+    STDERR.puts "WsmanPlugin.pull, op #{op}"
+  end
+  def self.get op
+    STDERR.puts "WsmanPlugin.get, op #{op}"
+  end
+  def self.custom op
+    STDERR.puts "WsmanPlugin.custom, op #{op}"
+  end
+  def self.put op
+    STDERR.puts "WsmanPlugin.put, op #{op}"
+  end
+  def self.create op
+    STDERR.puts "WsmanPlugin.create, op #{op}"
+  end
+  def self.delete op
+    STDERR.puts "WsmanPlugin.delete, op #{op}"
+  end
   class Sample
     SCHEMA = "http://schema.opensuse.org/swig/wsman-schema/1-0"
     def initialize *args
@@ -44,5 +74,5 @@ module Rbwsman
     def delete op
       STDERR.puts "WsmanPlugin.delete, op #{op}"
     end
-end
+  end
 end
