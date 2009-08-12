44e0b73c4babf2a4f21dc5bc752187e565331624
Klaus Kaempf <kkaempf@novell.com>
2009-08-12 08:43:49 +0000

    show set_dump_request() (commented out)

--- bindings/perl/tests/identify.pl
+++ bindings/perl/tests/identify.pl
@@ -6,7 +6,7 @@ use lib '..';
 use openwsman;
 
 # debug (to stderr)
-# openwsman::set_debug(1);
+# openwsman::set_debug(-1);
 
 # host, port, path, scheme, username, password
 $self->{CLIENT} = new openwsman::Client::('localhost', 5985, '/wsman', 'http', 'wsman', 'secret') or die print '<span class="error">[ERROR] Could not create client handler.</span><br/>';
@@ -15,6 +15,9 @@ $self->{CLIENT} = new openwsman::Client::('localhost', 5985, '/wsman', 'http', '
 # $self->{CLIENT} = new openwsman::Client::('http://wsman:secret@localhost:8889/wsman') or die print '<span class="error">[ERROR] Could not create client handler.</span><br/>';
 
 $self->{CLIENT_OPTIONS} = new openwsman::ClientOptions::() or die print '<span class="error">[ERROR] Could not create client options handler.</span><br/>';
+
+# Dump the XML request to stdout
+# $self->{CLIENT_OPTIONS}->set_dump_request();
 
 # force basic auth
 $self->{CLIENT}->transport()->set_auth_method($openwsman::BASIC_AUTH_STR);
