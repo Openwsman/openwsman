d6377ab716deff2039b8d4bbfba26bb272c821a8
Klaus Kaempf <kkaempf@novell.com>
2009-11-14 00:08:47 +0000

    add (commented out) debug and Windows (winrm) options

--- bindings/python/tests/identify.py
+++ bindings/python/tests/identify.py
@@ -6,10 +6,13 @@ from pywsman import *
 class TestSequenceFunctions(unittest.TestCase):
 
 	def test_identify(self):
+#               set_debug(1) # enable to print logging to stderr
 		client = Client( "http://wsman:secret@localhost:5985/wsman" )
 		assert client is not None
+#		client.transport().set_auth_method(BASIC_AUTH_STR) # Windows winrm needs this
 		options = ClientOptions()
 		assert options is not None
+#		options.set_dump_request() # enable to print SOAP request to stdout
 		doc = client.identify( options )
 		assert doc is not None
 		root = doc.root()
