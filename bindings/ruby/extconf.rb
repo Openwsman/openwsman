6fa5f3fd0a1009f116ef93d41bbb740398207d79
Klaus Kämpf <kkaempf@suse.de>
2012-02-02 16:32:55 +0100

    Bindings: First attempt on openwsman as a Gem in Ruby

---
+++ bindings/ruby/extconf.rb
@@ -0,0 +1,33 @@
+#
+# extconf.rb for openwsman Gem
+#
+
+require 'mkmf'
+# $CFLAGS = "#{$CFLAGS} -Werror"
+
+# requires wsman, wsman_client, and libxml2
+
+unless have_library('wsman', 'wsman_create_doc')
+  STDERR.puts "Cannot find wsman_create_doc() in libwsman"
+  STDERR.puts "Is openwsman-devel installed ?"
+  exit 1
+end
+find_header 'wsman-xml-api.h', '/usr/include/openwsman'
+
+unless have_library('wsman_client', 'wsmc_create')
+  STDERR.puts "Cannot find wsmc_create() in libwsman_client"
+  STDERR.puts "Is openwsman-devel installed ?"
+  exit 1
+end
+find_header 'wsman-client-api.h', '/usr/include/openwsman'
+
+unless have_library('xml2', 'xmlNewDoc')
+  STDERR.puts "Cannot find xmlNewDoc() in libxml2"
+  STDERR.puts "Is libxml2-devel installed ?"
+  exit 1
+end
+find_header 'libxml/parser.h', '/usr/include/libxml2'
+
+$CPPFLAGS = "-I/usr/include/openwsman -I.."
+
+create_makefile('openwsman')
