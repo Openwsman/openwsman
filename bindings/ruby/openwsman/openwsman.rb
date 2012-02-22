675432533b842cda022e991c7847c2873f38b493
Klaus Kämpf <kkaempf@suse.de>
2012-02-22 12:16:33 +0100

    Add Openwsman.epr_prefix_for to put all EPR prefixes in one place

--- bindings/ruby/openwsman/openwsman.rb
+++ bindings/ruby/openwsman/openwsman.rb
@@ -24,4 +24,35 @@ module Openwsman
       # return nil to abort authentication
     end
   end
+  #
+  # return EPR prefix for given classname (<schema>_<classname>)
+  #
+  def self.epr_prefix_for classname, namespace = nil
+    schema = classname.split("_")[0] rescue nil
+    return case schema
+    # dmtf CIM
+    when "CIM" then "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2"
+    # dmtf reserved
+    when "PRS" then "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2"
+    # Microsoft WMI
+    when "Win32"
+      prefix = "http://schemas.microsoft.com/wbem/wsman/1/wmi"
+      prefix += "/#{namespace}" if namespace
+      prefix
+    # openwbem.org
+    when "OpenWBEM" then "http://schema.openwbem.org/wbem/wscim/1/cim-schema/2"
+    # sblim
+    when "Linux" then "http://sblim.sf.net/wbem/wscim/1/cim-schema/2"
+    # omc-project
+    when "OMC" then "http://schema.omc-project.org/wbem/wscim/1/cim-schema/2"
+    # pegasus.org
+    when "PG" then "http://schema.openpegasus.org/wbem/wscim/1/cim-schema/2"
+    # Intel AMT
+    when "AMT" then "#http://intel.com/wbem/wscim/1/amt-schema/1"
+    # Intel
+    when "IPS" then "#http://intel.com/wbem/wscim/1/ips-schema/1"
+    else
+      raise "Unsupported schema #{schema.inspect} of class #{classname.inspect}"
+    end
+  end
 end
