039a31a3d90d44ac609846bcf888aee22100eb2b
Klaus Kämpf <kkaempf@suse.de>
2012-03-14 11:50:24 +0100

    v2.3.1

--- VERSION.cmake
+++ VERSION.cmake
@@ -1,4 +1,4 @@
-# ==================================================
+## ==================================================
 # Versioning
 # ==========
 #
@@ -47,7 +47,7 @@
 # Package version 2.3.0
 SET(OPENWSMAN_MAJOR "2")
 SET(OPENWSMAN_MINOR "3")
-SET(OPENWSMAN_PATCH "0")
+SET(OPENWSMAN_PATCH "1")
 
 # Plugin API 2.2
 SET(OPENWSMAN_PLUGIN_API_MAJOR "2")
