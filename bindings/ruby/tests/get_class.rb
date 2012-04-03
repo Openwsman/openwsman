10a1e931f0342d023527b23d14e6b6fc63463cc7
Klaus Kämpf <kkaempf@suse.de>
2012-04-03 16:01:44 +0200

    get_class: enhance output

--- bindings/ruby/tests/get_class.rb
+++ bindings/ruby/tests/get_class.rb
@@ -8,6 +8,30 @@ require File.join(File.dirname(__FILE__),'_loadpath')
 require 'openwsman'
 require '_client'
 
+def show_fault result
+  fault = Openwsman::Fault.new result
+  STDERR.puts "Fault code #{fault.code}, subcode #{fault.subcode}"
+  STDERR.puts "\treason #{fault.reason}"
+  STDERR.puts "\tdetail #{fault.detail}"
+end
+
+def show_error client
+  STDERR.puts "Client failed"
+  STDERR.puts "\tResult code #{client.response_code}, Fault: #{client.fault_string}"
+end
+
+def fault? client, result
+  if result
+    if result.fault?
+      show_fault result
+      true
+    end
+  else
+    show_error client
+    true
+  end
+end
+
 class WsmanTest < Test::Unit::TestCase
   def test_get_class
     Openwsman::debug = 1
@@ -17,12 +41,13 @@ class WsmanTest < Test::Unit::TestCase
 #    options.set_dump_request
     
     method = Openwsman::CIM_ACTION_GET_CLASS
-    uri = Openwsman::XML_NS_CIM_INTRINSIC + "/CIM_Service"
+    uri = Openwsman::XML_NS_CIM_INTRINSIC + "/CIM_ComputerSystem"
 #    uri = Openwsman::CIM_ALL_AVAILABLE_CLASSES
     
 #    options.properties = { "Prefix" => "Linux" }
 
     result = client.invoke( options, uri, method )
+    fault? client, result
     assert result
 #    print result
     unless result.fault?
@@ -31,7 +56,9 @@ class WsmanTest < Test::Unit::TestCase
       qualifiers = node["qualifiers"]
       properties = node["properties"]
       puts "Class #{name} with #{qualifiers.size} Qualifiers and #{properties.size} Properties"
-
+      puts result.to_xml
+      qualifiers.each { |q| puts q.to_xml }
+      properties.each { |p| puts p.to_xml }
     else
       puts "--Fault--"
     end
