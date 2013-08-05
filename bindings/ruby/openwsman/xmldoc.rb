761a373afe7439c1a46ff7ac10990d66d4c2bc26
Klaus Kämpf <kkaempf@suse.de>
2013-08-05 15:18:42 +0200

    Move comment to right indent

--- bindings/ruby/openwsman/xmldoc.rb
+++ bindings/ruby/openwsman/xmldoc.rb
@@ -1,9 +1,8 @@
-#
-# Assume XmlDoc.foo means XmlDoc.body.foo
-#
-
 module Openwsman
   class XmlDoc
+    #
+    # Assume XmlDoc.foo means XmlDoc.body.foo
+    #
     def method_missing method, *args
       self.body.send method,*args
     end
