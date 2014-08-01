4d156d0edab32e4feadfc09ad0f54184fa35775b
Klaus Kämpf <kkaempf@suse.de>
2014-08-01 11:47:46 +0200

    v2.4.11

--- VERSION.cmake
+++ VERSION.cmake
@@ -44,10 +44,10 @@
 #    set COMPATMINOR to MINOR. (binary incompatible change)
 #
 
-# Package version 2.4.9
+# Package version 2.4.11
 SET(OPENWSMAN_MAJOR "2")
 SET(OPENWSMAN_MINOR "4")
-SET(OPENWSMAN_PATCH "10")
+SET(OPENWSMAN_PATCH "11")
 
 # Plugin API 2.2
 SET(OPENWSMAN_PLUGIN_API_MAJOR "2")
