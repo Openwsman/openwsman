43e2b75d325419f959b7030981ca70cdeb56ba54
Peter Hatina <phatina@redhat.com>
2015-02-17 09:33:14 +0100

    refactor WsmanEPR class

--- src/cpp/WsmanEPR.h
+++ src/cpp/WsmanEPR.h
@@ -7,38 +7,26 @@
 using namespace std;
 
 namespace WsmanClientNamespace {
-	
+
 class WsmanEPR {
 private:
 	epr_t *epr;
+
 public:
-	WsmanEPR(const string &eprstring) {
-		epr = epr_from_string(eprstring.c_str());
-	}
-	WsmanEPR(const string &uri, const string &address) {
-		epr = epr_create(uri.c_str(), NULL, address.c_str());
-	}
-	WsmanEPR(WsmanEPR &epr) {
-		this->epr = epr_copy(epr.getepr());
-	}
-	~WsmanEPR() {
-		epr_destroy(this->epr);
-	};
-	int addTextSelector(const string &name, const string &value) {
-		return epr_add_selector_text(this->epr, name.c_str(), value.c_str());
-	}
-	int addEprSelector(const string &name, WsmanEPR &epr) {
-		return epr_add_selector_epr(this->epr, name.c_str(), epr.getepr());
-	}
-	int deleteSelector(const string &name) {
-		return epr_delete_selector(this->epr, name.c_str());
-	}
-	int compare(WsmanEPR *epr1, WsmanEPR *epr2) {
-		return epr_cmp(epr1->getepr(), epr2->getepr());
-	}
-	epr_t *getepr() {
-		return this->epr;
-	}
+	WsmanEPR(const string &eprstring);
+	WsmanEPR(const string &uri, const string &address);
+	WsmanEPR(WsmanEPR &epr);
+	~WsmanEPR();
+
+	int addTextSelector(const string &name, const string &value);
+	int addEprSelector(const string &name, WsmanEPR &epr);
+	int deleteSelector(const string &name);
+
+	bool operator==(const WsmanEPR &rhs) const;
+	bool operator!=(const WsmanEPR &rhs) const;
+
+	epr_t *getepr() const;
+	operator epr_t*() const;
 };
 
 }
