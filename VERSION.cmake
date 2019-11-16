a165bfd00ac9b9ec5e6ec27136248243e1c14ea9
Klaus Kämpf <kkaempf@suse.de>
2019-11-16 14:00:09 +0100

    Version 2.7.0, libwsman_client.so.5

--- VERSION.cmake
+++ VERSION.cmake
@@ -44,10 +44,10 @@
 #    set COMPATMINOR to MINOR. (binary incompatible change)
 #
 
-# Package version 2.6.11
+# Package version 2.7.0
 SET(OPENWSMAN_MAJOR "2")
-SET(OPENWSMAN_MINOR "6")
-SET(OPENWSMAN_PATCH "12")
+SET(OPENWSMAN_MINOR "7")
+SET(OPENWSMAN_PATCH "0")
 
 # Plugin API 2.2
 SET(OPENWSMAN_PLUGIN_API_MAJOR "2")
