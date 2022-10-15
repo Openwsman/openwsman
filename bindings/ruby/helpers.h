8a94f05929fd8ca3ea67391c3d406a3bbd61edb8
Klaus Kämpf <kkaempf@suse.de>
2022-10-15 10:38:14 +0200

    fix function args for rb_hash_foreach
    Signed-off-by: Klaus Kämpf <kkaempf@suse.de>

--- bindings/ruby/helpers.h
+++ bindings/ruby/helpers.h
@@ -110,8 +110,9 @@ hash2value( hash_t *hash )
  *  (used as callback for value2hash)
  */
 static int
-_add_str( VALUE key, VALUE value, hash_t *h )
+_add_str( VALUE key, VALUE value, VALUE opaque )
 {
+    hash_t *h = (hash_t *)opaque;
     if (key != Qundef) {
 	const char *k = strdup( as_string( key ) );
 	if (!hash_lookup( h, k ) ) {
@@ -129,8 +130,9 @@ _add_str( VALUE key, VALUE value, hash_t *h )
  *  (used as callback for value2hash)
  */
 static int
-_add_kv( VALUE key, VALUE value, hash_t *h )
+_add_kv( VALUE key, VALUE value, VALUE opaque )
 {
+    hash_t *h = (hash_t *)opaque;
     if (key != Qundef) {
 	const char *k = as_string( key );
 	if (!hash_lookup( h, k ) ) {
