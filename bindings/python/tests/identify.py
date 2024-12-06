c2387896274ffe4547b0daaaec9ca7af24450776
Klaus Kämpf <kkaempf@gmail.com>
2024-12-06 14:54:01 +0100

    Update to Python3 syntax
    Signed-off-by: Klaus Kämpf <kkaempf@gmail.com>

--- bindings/python/tests/identify.py
+++ bindings/python/tests/identify.py
@@ -14,19 +14,19 @@ from pywsman import *
 class TestSequenceFunctions(unittest.TestCase):
 
    def test_identify(self):
-#     set_debug(1) # enable to print logging to stderr
+#      set_debug(1) # enable to print logging to stderr
       client = Client( "http://wsman:secret@localhost:5985/wsman" )
       assert client is not None
       client.transport().set_auth_method(BASIC_AUTH_STR) # Windows winrm needs this
       options = ClientOptions()
       assert options is not None
-#     options.set_dump_request() # enable to print SOAP request to stdout
+#      options.set_dump_request() # enable to print SOAP request to stdout
       doc = client.identify( options )
       assert doc is not None
-#      print "Document [%s]" % doc
+#      print("Document [%s]" % doc)
       root = doc.root()
       assert root is not None
-#      print "Root [%s]" % root
+#      print("Root [%s]" % root)
       prot_version = root.find( XML_NS_WSMAN_ID, "ProtocolVersion" )
       prod_vendor = root.find( XML_NS_WSMAN_ID, "ProductVendor" )
       prod_version = root.find( XML_NS_WSMAN_ID, "ProductVersion" )
