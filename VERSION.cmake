6c83743ee3614866698db43488cc7b141def8844
Klaus Kämpf <kkaempf@gmail.com>
2024-12-06 15:26:26 +0100

    Bump to 2.8.0
    Signed-off-by: Klaus Kämpf <kkaempf@gmail.com>

--- VERSION.cmake
+++ VERSION.cmake
@@ -44,10 +44,10 @@
 #    set COMPATMINOR to MINOR. (binary incompatible change)
 #
 
-# Package version 2.7.2
+# Package version 2.8.0
 SET(OPENWSMAN_MAJOR "2")
-SET(OPENWSMAN_MINOR "7")
-SET(OPENWSMAN_PATCH "2")
+SET(OPENWSMAN_MINOR "8")
+SET(OPENWSMAN_PATCH "0")
 
 # Plugin API 2.2
 SET(OPENWSMAN_PLUGIN_API_MAJOR "2")
